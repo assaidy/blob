5911f40baf1d3a47ec01eb8907a12ead366f8dcf
Ahmad Assaidy <alsaidyxcode@gmail.com>
2025-01-11 21:16:21 +0200

    following standards with APIError impl for error interface
    by implementing Error() for *APIError.
    used std libaray instead of fiber to limit dependencies.

--- api_error.go
+++ api_error.go
@@ -1,6 +1,6 @@
 package blob
 
-import "github.com/gofiber/fiber/v2"
+import "net/http"
 
 type APIError struct {
 	Code          int    `json:"-"`
@@ -9,67 +9,67 @@ type APIError struct {
 	InternalError error  `json:"-"`
 }
 
-func (e APIError) Error() string {
+func (e *APIError) Error() string {
 	if e.InternalError == nil {
 		return e.Message
 	}
 	return e.InternalError.Error()
 }
 
-func InvalidJsonRequestError() APIError {
-	return APIError{
-		Code:    fiber.StatusBadRequest,
+func InvalidJsonRequestError() *APIError {
+	return &APIError{
+		Code:    http.StatusBadRequest,
 		Message: "failed to parse request body",
 	}
 }
 
-func BadRequestError(msg string) APIError {
-	return APIError{
-		Code:    fiber.StatusBadRequest,
+func BadRequestError(msg string) *APIError {
+	return &APIError{
+		Code:    http.StatusBadRequest,
 		Message: msg,
 	}
 }
 
-func InvalidDataError(msg string) APIError {
-	return APIError{
-		Code:    fiber.StatusUnprocessableEntity,
+func InvalidDataError(msg string) *APIError {
+	return &APIError{
+		Code:    http.StatusUnprocessableEntity,
 		Message: msg,
 	}
 }
 
-func ValidationError(errs any) APIError {
-	return APIError{
-		Code:    fiber.StatusUnprocessableEntity,
+func ValidationError(errs any) *APIError {
+	return &APIError{
+		Code:    http.StatusUnprocessableEntity,
 		Message: "invalid request data",
 		Errors:  errs,
 	}
 }
 
-func ConflictError(msg string) APIError {
-	return APIError{
-		Code:    fiber.StatusConflict,
+func ConflictError(msg string) *APIError {
+	return &APIError{
+		Code:    http.StatusConflict,
 		Message: msg,
 	}
 }
 
-func NotFoundError(msg string) APIError {
-	return APIError{
-		Code:    fiber.StatusNotFound,
+func NotFoundError(msg string) *APIError {
+	return &APIError{
+		Code:    http.StatusNotFound,
 		Message: msg,
 	}
 }
 
-func InternalServerError(err error) APIError {
-	return APIError{
-		Code:          fiber.StatusInternalServerError,
+func InternalServerError(err error) *APIError {
+	return &APIError{
+		Code:          http.StatusInternalServerError,
 		Message:       "internal server error",
 		InternalError: err,
 	}
 }
 
-func UnauthorizedError() APIError {
-	return APIError{
-		Code:    fiber.StatusUnauthorized,
-		Message: fiber.ErrUnauthorized.Message,
+func UnauthorizedError() *APIError {
+	return &APIError{
+		Code:    http.StatusUnauthorized,
+		Message: "unauthorized",
 	}
 }
